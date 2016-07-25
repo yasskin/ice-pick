2955cb89240c05437892d692da58a69faa1a3df6
August von Trapp and Noah Yasskin <noah@>
2016-07-25 12:03:29 -0700

    add callback method for User class that downcases all inputs

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
+  before_save(:downcase_user)
 
+private
+  def :downcase_user do
+    self.first_name().downcase!
+    self.last_name().downcase!
+    self.track().downcase!
+    self.transportation().downcase!
+    self.lunch().downcase!
+    self.ninth_floor().downcase!
+  end
 end
