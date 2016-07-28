0c216280775130d02af1954229f355d1ce6e25d1
Marty Fitchen and Maarja Laineste <marty@>
2016-07-28 10:01:45 -0700

    updated seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+require('./lib/player')
+require('./lib/question')
+
+seed_users = ["", ""]
+
+seed_users.each do |first|
+
+
+string   "first_name"
+string   "last_name"
+string   "track"
+string   "transportation"
+string   "lunch"
+string   "ninth_floor"
+string   "photo"
+string   "sleeping"
+string   "temperature"
+string   "pokemon"
