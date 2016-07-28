f1e0a061c938e8e4dd495e82297509cfee49bc13
Marty Fitchen and Maarja Laineste <maarja@>
2016-07-28 16:55:43 -0700

    fix margin for nametag animation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 require './lib/question'
 
-Question.create(prompt: 'Which person is ', topic: 'first_name', target: '')
-Question.create(prompt: 'Which person goes to food carts?', topic: 'lunch', target: 'carts')
-Question.create(prompt: 'Which person is doing', topic: 'track', target: 'c#')
-Question.create(prompt: 'Which person is doing', topic: 'track', target: 'css')
-Question.create(prompt: 'Which person has been to 9th floor bathroom?', topic: 'ninth_floor', target: 'yes')
-Question.create(prompt: 'Which person plays Pokémon Go?', topic: 'pokemon', target: 'yes')
-Question.create(prompt: 'Which person rides their bike to school?', topic: 'transportation', target: 'bike')
-Question.create(prompt: 'Which person has napped or slept on the Epicodus sofas?', topic: 'sleeping', target: 'yes')
+Question.create(prompt: 'Which human is ', topic: 'first_name', target: '')
+Question.create(prompt: 'Which human goes to food carts?', topic: 'lunch', target: 'carts')
+Question.create(prompt: 'Which human is doing', topic: 'track', target: 'c#')
+Question.create(prompt: 'Which human is doing', topic: 'track', target: 'css')
+Question.create(prompt: 'Which human has been to 9th floor bathroom?', topic: 'ninth_floor', target: 'yes')
+Question.create(prompt: 'Which human plays Pokémon Go?', topic: 'pokemon', target: 'yes')
+Question.create(prompt: 'Which human rides their bike to school?', topic: 'transportation', target: 'bike')
+Question.create(prompt: 'Which human has napped or slept on the Epicodus sofas?', topic: 'sleeping', target: 'yes')
