75d2e804d7bc433008b276795a8bc48653fb3825
Augustine von Trapp <Augustvontrapp@gmail.com>
2016-07-28 08:42:57 -0700

    first merge with master on Thursday

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,20 +1,24 @@
 $( document ).ready(function() {
+
   $('.correct').on('submit', function(e){
     var form = this;
     e.preventDefault();
-    $('body').prepend('<div><h1 style="font-size: 6em">YOU WIN!!!!</h1></div>').fadeOut
+    $('body').prepend('<div class="win"><h1 style="font-size: 6em">YOU WIN!!!!</h1></div>')
+    $('.win').fadeOut(1000);
      setTimeout(function() {
         console.log("yeah");
         form.submit();
-     }, 300);
+     }, 1500);
    });
+
   $('.wrong').on('submit', function(e){
     var form = this;
     e.preventDefault();
-    $('body').prepend('<div style="font-size: 6em"><h1>YOU LOSE!!!!</h1></div>').fadeOut
+    $('body').prepend('<div class="lose"><h1 style="font-size: 6em">YOU LOSE!!!!</h1></div>')
+    $('.lose').fadeOut(1000);
      setTimeout(function() {
         console.log("yeah");
         form.submit();
-     }, 300);
+     }, 1500);
    });
 });
